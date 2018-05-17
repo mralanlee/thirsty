e2be50e31e0430d81ae25035d1acd0a51bc927ff
Alan Lee <alan@pointapp.org>
2018-05-16 21:42:16 -0700

    update to export function

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	v "thirsty/validate"
+
 	"gopkg.in/urfave/cli.v1"
 )
 
@@ -8,6 +13,15 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "thirsty"
 	app.Action = func(c *cli.Context) error {
+		result := v.CheckEnv(c.Args().Get(0))
 
+		fmt.Println(result)
+		return nil
 	}
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
--- validate/validate.go
+++ validate/validate.go
@@ -2,7 +2,8 @@ package validate
 
 import "strings"
 
-func checkEnv(environment string) bool {
+// CheckEnv If proper environment is provided in flag
+func CheckEnv(environment string) bool {
 	env := strings.ToLower(environment)
 	return env != "qa" && env != "dev"
 }
